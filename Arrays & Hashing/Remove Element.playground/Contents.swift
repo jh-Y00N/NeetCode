644213f7fdf0b2fe7dd898533f1e37c772a66071
jh-Y00N <jhyoon7109@gmail.com>
2025-10-17 17:53:23 -0700

    27. Remove Element fix

--- Arrays & Hashing/Remove Element.playground/Contents.swift
+++ Arrays & Hashing/Remove Element.playground/Contents.swift
@@ -1,10 +1,14 @@
 import Foundation
 
 func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
-    nums.removeAll { num in
-        return num == val
+    var k = 0
+    for i in 0..<nums.count {
+        if nums[i] != val {
+            nums[k] = nums[i]
+            k += 1
+        }
     }
-    return nums.count
+    return k
 }
 
 var numbers = [0,1,2,2,3,0,4,2]
