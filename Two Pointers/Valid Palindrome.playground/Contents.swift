c8fa4448166ddc2561678dba52675a56e061a475
jh-Y00N <jhyoon7109@gmail.com>
2025-11-05 15:50:13 -0800

    125. Valid Palindrome extra

--- Two Pointers/Valid Palindrome.playground/Contents.swift
+++ Two Pointers/Valid Palindrome.playground/Contents.swift
@@ -1,9 +1,25 @@
 import Foundation
 
 func isPalindrome(_ s: String) -> Bool {
-    var trimmedStr = s.lowercased().components(separatedBy: .alphanumerics.inverted).joined()
-    var reversedStr = String(trimmedStr.reversed())
-    return trimmedStr == reversedStr
+    /*
+     var trimmedStr = s.lowercased().components(separatedBy: .alphanumerics.inverted).joined()
+     var reversedStr = String(trimmedStr.reversed())
+     return trimmedStr == reversedStr
+     */
+    
+    var trimmedStr = Array(s.filter({ $0.isLetter || $0.isNumber }))
+    var i = 0
+    var j = trimmedStr.count - 1
+    
+    while i < j {
+        if trimmedStr[i].lowercased() != trimmedStr[j].lowercased() {
+            return false
+        } else {
+            i += 1
+            j -= 1
+        }
+    }
+    return true
 }
 
-print(isPalindrome("race a car"))
+print(isPalindrome("A man, a plan, a canal: Panama"))
