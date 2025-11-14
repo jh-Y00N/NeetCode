adbfd2ec7b22d3474aaa3dbc5eec08f9ed055b7f
jh-Y00N <jhyoon7109@gmail.com>
2025-11-13 16:50:03 -0800

    167. Two Sum II - Input Array Is Sorted extra

--- Two Pointers/Two Sum II - Input Array Is Sorted.playground/Contents.swift
+++ Two Pointers/Two Sum II - Input Array Is Sorted.playground/Contents.swift
@@ -1,9 +1,23 @@
 import Foundation
 
 func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
-    for i in 0..<numbers.count {
-        if let index = numbers.lastIndex(of: target - numbers[i]) {
-            return [i + 1, index + 1]
+    /*
+     for i in 0..<numbers.count {
+         if let index = numbers.lastIndex(of: target - numbers[i]) {
+             return [i + 1, index + 1]
+         }
+     }
+     return []
+     */
+    var i = 0, j = numbers.count - 1
+    while i < j {
+        let sum = numbers[i] + numbers[j]
+        if sum == target {
+            return [i + 1, j + 1]
+        } else if sum < target {
+            i += 1
+        } else {
+            j -= 1
         }
     }
     return []
