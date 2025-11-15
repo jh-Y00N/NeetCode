3e8ebdbe768848a6636b3755d7b6b2edff9dfd02
jh-Y00N <jhyoon7109@gmail.com>
2025-11-14 17:11:40 -0800

    881. Boats to Save People

---
+++ Two Pointers/Boats to Save People.playground/Contents.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+func numRescueBoats(_ people: [Int], _ limit: Int) -> Int {
+    var count = people.count(where: {$0 == limit })
+    var peopleLeft = people.filter( { $0 != limit })
+    
+    var left = 0, right = peopleLeft.count - 1
+    peopleLeft.sort()
+    
+    while peopleLeft.count > 1, left < right {
+        if peopleLeft[left] + peopleLeft[right] > limit {
+            right -= 1
+        } else if peopleLeft[left] + peopleLeft[right] == limit {
+            count += 1
+            peopleLeft.remove(at: right)
+            peopleLeft.remove(at: left)
+            right -= 2
+        } else {
+            count += 1
+            peopleLeft.remove(at: right)
+            peopleLeft.remove(at: left)
+            right -= 2
+        }
+    }
+    if peopleLeft.count == 2, peopleLeft[0] + peopleLeft[1] <= limit {
+        count += 1
+        peopleLeft.removeAll()
+    }
+    
+    return count + peopleLeft.count
+}
+
+print(numRescueBoats([3,2,3,2,2], 6))
