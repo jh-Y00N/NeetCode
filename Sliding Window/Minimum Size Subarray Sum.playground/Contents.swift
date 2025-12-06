d30503237354085e8cac591cdd04ea6feabed161
jh-Y00N <jhyoon7109@gmail.com>
2025-12-05 18:46:23 -0800

    209. Minimum Size Subarray Sum fix

--- Sliding Window/Minimum Size Subarray Sum.playground/Contents.swift
+++ Sliding Window/Minimum Size Subarray Sum.playground/Contents.swift
@@ -1,26 +1,17 @@
 import Foundation
 
 func minSubArrayLen(_ target: Int, _ nums: [Int]) -> Int {
-    var l = 0, r = 1, sum = nums[0], length = nums.count + 1
+    var l = 0, sum = 0, length = nums.count + 1
     
-    if sum >= target { return 1 }
-    if nums.count == 1, sum < target { return 0 }
-    
-    sum += nums[r]
-    while r < nums.count, l <= r {
-        if sum < target {
-            r += 1
-            if r == nums.count {
-                return length == nums.count + 1 ? 0 : length
-            }
-            sum += nums[r]
-        } else {
+    for r in 0..<nums.count {
+        sum += nums[r]
+        while sum >= target {
             length = min(length, r - l + 1)
             sum -= nums[l]
             l += 1
         }
     }
-    return length
+    return length == nums.count + 1 ? 0 : length
 }
 
-print(minSubArrayLen(7, [2,3,1,2,4,3]))
+print(minSubArrayLen(6, [10,2,3]))
