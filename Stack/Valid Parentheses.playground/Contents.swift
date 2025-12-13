319d734fe83e6e3397b3fee1ab5ee6ca89ea2d80
jh-Y00N <jhyoon7109@gmail.com>
2025-12-12 21:11:55 -0800

    20. Valid Parentheses

---
+++ Stack/Valid Parentheses.playground/Contents.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+func isValid(_ s: String) -> Bool {
+    if s.count == 1 {
+        return false
+    }
+    
+    var sArr = Array(s)
+    if sArr[0] == ")" || sArr[0] == "}" || sArr[0] == "]" {
+        return false
+    }
+    
+    var charStack = [Character]()
+    charStack.append(sArr[0])
+    for i in 1..<sArr.count {
+        if sArr[i] == ")", charStack.last == "(" {
+            charStack.popLast()
+        } else if sArr[i] == "}", charStack.last == "{" {
+            charStack.popLast()
+        } else if sArr[i] == "]", charStack.last == "[" {
+            charStack.popLast()
+        } else {
+            charStack.append(sArr[i])
+        }
+    }
+    
+    return charStack.isEmpty
+}
+
+print(isValid("(])"))
