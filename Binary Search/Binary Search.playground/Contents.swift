f78cf2c91a8e8acfdc971461b2a02986682209f3
jh-Y00N <jhyoon7109@gmail.com>
2026-03-12 15:36:38 -0700

    704. Binary Search

---
+++ Binary Search/Binary Search.playground/Contents.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+func search(_ nums: [Int], _ target: Int) -> Int {
+    var start = 0
+    var end = nums.count - 1
+    
+    while start >= 0, end < nums.count, start <= end {
+        var index = (start + end) / 2
+        if nums[index] < target {
+            start = index + 1
+        } else if nums[index] > target {
+            end = index - 1
+        } else {
+            return index
+        }
+    }
+    return -1
+}
+
+print(search([-1,0,3,5,9,12], 9))
+print(search([-1,0,3,5,9,12], 2))
